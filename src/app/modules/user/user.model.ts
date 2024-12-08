9dc51c9c1d61f977e31520dbed276f1fc8d17035
TechBuilderTeam <saashid18@gmail.com>
2024-12-08 19:12:42 +0600

    login hashed password issue is fixed

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,20 +1,35 @@
 // user/model.ts
 import mongoose, { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt';
 import { IUser } from './user.interface';
 
 const userSchema = new Schema<IUser>({
-  name: { type: String, required: true},
+  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, select: false },
-  imageUrl: { type: String , required: false},
-  phone: { type: String , required: false},
-  address: { type: String , required: false },
+  imageUrl: { type: String },
+  phone: { type: String },
+  address: { type: String },
   role: { type: String, enum: ['user', 'admin'], default: 'user' },
   isBlocked: { type: Boolean, default: false },
   followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  premiumMembership: { type: Boolean, default: false }
+  premiumMembership: { type: Boolean, default: false },
 });
+
+// Pre-save hook to hash the password
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Method to compare passwords
+userSchema.methods.isCorrectPassword = async function (password: string): Promise<boolean> {
+  return bcrypt.compare(password, this.password);
+};
 
 const UserModel = model<IUser>('User', userSchema);
 export default UserModel;
